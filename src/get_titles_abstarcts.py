73cd99b0b08cc48890f1c949cf225575ee34a0a6
Daniel Tesfai <dannyhd88@gmail.com>
2026-08-06 13:24:02 +0200

    Remove get_titles_abstarcts.py script and its functionality

--- src/get_titles_abstarcts.py
+++
@@ -1,9 +0,0 @@
-
-import json
-
-with open("reference-repo/data/assets_example/metadata.json", "r") as f:
-    publications = json.load(f)
-
-for i in range(0,100):
-    print(publications[i]["title"])
-    print()
